727b2d77fdc782fae2c571ec1230a1a0a3aca721
NourhanAli10 <nourhan.ali.rezk92@gmail.com>
2024-05-08 23:00:27 +0300

    task 3

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,1 +1,11 @@
 -- SQL script that lists all bands with Glam rock as their main style, ranked by their longevity
+SELECT 
+    band_name,
+    IFNULL(TIMESTAMPDIFF(YEAR, formed, split), 0) AS lifespan
+FROM 
+    metal_bands
+WHERE
+    style = "Glam rock"
+ORDER BY 
+    lifespan DESC;
+
